8d3ffdc673f6c514957016232aa693077346bd65
Hokk123 <vladimir_vs@list.ru>
2025-08-06 14:04:53 +0300

    4

--- ExelBot.py
+++ ExelBot.py
@@ -20,7 +20,7 @@ def load_orders():
 
 @bot.message_handler(commands=['start', 'help'])
 def help(message: telebot.types.Message):
-    text = 'Чтобы начать работу, введите команду боту в следующем формате:имя файла в формате xls'
+    text = 'Чтобы начать работу, введите команду боту в следующем формате:имя файла в формате xlsx'
     bot.reply_to(message, text)
 
 # @bot.message_handler()
