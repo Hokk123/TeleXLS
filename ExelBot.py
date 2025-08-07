4acaccd2f6503997ff6bed07bc0ca172c9ce6d88
Hokk123 <vladimir_vs@list.ru>
2025-08-07 12:06:49 +0300

    1

--- ExelBot.py
+++ ExelBot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 from config import TOKEN
 import pandas as pd
 from datetime import datetime
@@ -10,48 +11,52 @@ year_now = datetime.now().year
 month_now = datetime.now().month
 
 # Загружаем данные из Excel-таблицы
-def load_orders():
+def load_data():
     # Укажите путь к вашей таблице Excel
-    excel_file = 'test.xlsx'
+    excel_file = 'mrg.xlsx'
 
     # Имя листа в таблице, где хранятся данные
     sheet_name = 'Лист1'
 
     # Загружаем данные из Excel-таблицы в DataFrame
     df = pd.read_excel(excel_file, sheet_name=sheet_name)
-    df['номер'] = df['номер'].astype(str)
     return df
-
-@bot.message_handler(commands=['start', 'help'])
-def help(message: telebot.types.Message):
-    text = 'Добро пожаловать! Этот бот предназначен для предоставления вам данных по реализации проекта системы связи и телекоммуникаций (ССиТ) ООО «Газпром межрегионгаз» (МРГ): номер записи'
-    bot.reply_to(message, text)
+    
+# Стартовое приветствие
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Статистика проекта")
+    btn2 = types.KeyboardButton("Планы проекта")
+    markup.add(btn1, btn2)
+    bot.send_message(message.chat.id, text="Добро пожаловать! Этот бот предназначен для предоставления вам данных по реализации проекта системы связи и телекоммуникаций (ССиТ) ООО «Газпром межрегионгаз» (МРГ):".format(message.from_user), reply_markup=markup)
 
 # @bot.message_handler()
 # def send_welcome(message):
 #     bot.send_message(message.chat.id, f"Welcome, {message.chat.username}")
 
 # Обработчик текстовых сообщений
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(content_types=['text'])
 def handle_text(message):
-    try:
-        val = int(message.text)
 
-        order_number = message.text.strip()
+    # Загружаем данные из DataFrame
+    data_df = load_data()
 
-        # Загружаем данные из Excel-таблицы
-        orders_df = load_orders()
-
-        # Ищем данные по номеру
-        status = orders_df.loc[orders_df['номер'] == order_number, 'данные'].values
-
-        if len(status) > 0:
-            bot.reply_to(message, f"{year_now} {month_now} данные {order_number}: {status[0]}")
+    if(message.text == "Статистика проекта"):
+        # Выводим данные - Всего
+        rows = len(data_df.index) #Количество строк в таблице
+        total = data_df.iloc[(rows-1),2]
+        if (total == 1):
+            bot.send_message(message.chat.id, f"{year_now} {month_now} На текущий момент работы выполнены на {total} объекте")
         else:
-            bot.reply_to(message, f"данные с номером {order_number} не найдены.")
-            
-    except ValueError:
-        print("That's not an int!")
+            bot.send_message(message.chat.id, f"{year_now} {month_now} На текущий момент работы выполнены на {total} объектах")
+    elif(message.text == "Планы проекта"):
+        # Ищем данные по текущему месяцу
+        
+        bot.send_message(message.chat.id, text="Привеет.. Вывод планов в процессе создания!)")
+        
+        
+
 
 # Запускаем бота
 bot.polling()
